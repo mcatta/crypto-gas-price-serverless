743424ea23c172a7ba6d9195ed3a19b83632cf74
Marco Cattaneo <marcocatta@gmail.com>
2021-04-11 21:51:25 +0200

    fix: changed api and stored data

--- functions/gas_fee/gas_fee_api.js
+++ functions/gas_fee/gas_fee_api.js
@@ -1,27 +1,20 @@
 const axios = require('axios').default
 const functions = require("firebase-functions");
-require('dotenv').config()
 
 
 module.exports = (_store) => {
 	let modules = []
 
 	modules.check = () => {
-		return axios.get('https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=' + process.env.DEFIPULSE_API_KEY).then((response) => {
-			let data = response.data
+		return axios.get('https://www.gasnow.org/api/v3/gas/price?utm_source=android').then((response) => {
+			let data = response.data.data
 
 			return _store.add({
 					fast: data.fast,
-					fastest: data.fastest,
-					safeLow: data.safeLow,
-					average: data.average,
-					block_time: data.block_time,
-					blockNum: data.blockNum,
-					speed: data.speed,
-					safeLowWait: data.safeLowWait,
-					avgWait: data.avgWait,
-					fastWait: data.fastWait,
-					fastestWait: data.fastestWait
+					rapid: data.rapid,
+					standard: data.standard,
+					slow: data.slow,
+					timestamp: data.timestamp
 			})
 		}).catch(function (error) {
 			// handle error
